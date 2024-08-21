cc37613243fbb1a99196320cd989b13f54f1caed
Kliment <47915301+xKliment@users.noreply.github.com>
2024-08-21 18:20:25 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/x4UvIrs0qcnPwlNH/scene.splinecode" />
+  );
+}
